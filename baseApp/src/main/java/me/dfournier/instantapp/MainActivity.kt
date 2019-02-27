19a7e6a9f054958f3be3cd333ceb3a3f951111f0
Fournier Donovan <support@dfournier.me>
2019-02-27 22:36:19 +0100

    Remove MultiFeature

--- baseApp/src/main/java/me/dfournier/instantapp/MainActivity.kt
+++ baseApp/src/main/java/me/dfournier/instantapp/MainActivity.kt
@@ -1,33 +1,26 @@
 package me.dfournier.instantapp
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import com.google.android.play.core.splitinstall.SplitInstallManager
-import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    val manager: SplitInstallManager by lazy {
-        SplitInstallManagerFactory.create(this)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         account_button.setOnClickListener {
+/*
             startActivity(
                 Intent(Intent.ACTION_VIEW)
                     .setClassName(this, "me.dfournier.instantapp.account_management.AccountListActivity")
             )
+*/
         }
 
         stock_button.setOnClickListener {
+/*
             try {
                 startActivity(
                     Intent(Intent.ACTION_VIEW)
@@ -36,6 +29,7 @@ class MainActivity : AppCompatActivity() {
             } catch (e: ActivityNotFoundException) {
                 Toast.makeText(this, "Activity not found", Toast.LENGTH_SHORT).show()
             }
+*/
         }
     }
 }
