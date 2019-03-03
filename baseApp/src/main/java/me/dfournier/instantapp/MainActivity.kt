7dc79f912ecdca7b4ad3ac7c92681a14666f6b24
d-fournier <support@dfournier.me>
2019-03-03 19:04:15 +0100

    Add Dynamic delivery

--- baseApp/src/main/java/me/dfournier/instantapp/MainActivity.kt
+++ baseApp/src/main/java/me/dfournier/instantapp/MainActivity.kt
@@ -3,10 +3,20 @@ package me.dfournier.instantapp
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.google.android.gms.instantapps.InstantApps
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+
+    val splitInstallManager: SplitInstallManager by lazy {
+        SplitInstallManagerFactory.create(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +42,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         stock_button.setOnClickListener {
-//            startActivity(
-//                Intent(Intent.ACTION_VIEW)
-//                    .setClassName(this, "me.dfournier.instantapp.subscription.StockActivity")
-//            )
+            val moduleName = getString(R.string.title_stock)
+            val intent =
+                Intent(Intent.ACTION_VIEW).setClassName(this, "me.dfournier.instantapp.subscription.StockActivity")
+
+            if (splitInstallManager.installedModules.contains(moduleName)) {
+                startActivity(intent)
+            } else {
+                stock_loading.visibility = View.VISIBLE
+                stock_error.visibility = View.GONE
+                stock_button.isEnabled = false
+                val request = SplitInstallRequest
+                    .newBuilder()
+                    .addModule(moduleName)
+                    .build()
+                splitInstallManager.startInstall(request)
+                    .addOnSuccessListener {
+                        stock_loading.visibility = View.GONE
+                        stock_button.isEnabled = true
+                        startActivity(intent)
+                    }
+                    .addOnFailureListener {
+                        stock_loading.visibility = View.GONE
+                        stock_error.visibility = View.VISIBLE
+                        stock_button.isEnabled = true
+                    }
+            }
         }
     }
 }
